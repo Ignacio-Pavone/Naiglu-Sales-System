6515129181fd2c093dd38705310e5f4364cb18c2
whileNazaTrue <nazarenoorodriguez@gmail.com>
2022-05-20 14:04:43 -0300

    More refactorin

---
+++ src/DatabaseRelated/Inventory.java
@@ -0,0 +1,45 @@
+package DatabaseRelated;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+public class Product extends JDialog {
+
+    private JPanel products1;
+    private JTabbedPane tabbedPane1;
+    private JButton exitButton;
+    private JButton button2;
+    private JTable tablaProductos;
+    private Product productData;
+    private HashMap<Integer,Product> productList = new HashMap<>();
+
+    public Product(JFrame parent) {
+        super(parent);
+        crearTabla();
+        setMinimumSize(new Dimension(400, 450));
+        setContentPane(products1);
+        setModal(true);
+        setUndecorated(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+    private void crearTabla (){
+        tablaProductos.setModel(new DefaultTableModel(
+                null,
+                new String [] {"ID", "NOMBRE", "STOCK","PRECIO"}
+
+        ));
+    };
+
+
+}
